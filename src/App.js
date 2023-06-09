474c33b64e73a7771f49761c563d3e91b682a460
Macolister <bispo.mac@gmail.com>
2023-06-09 17:19:28 -0400

    make domContent component, add ids and classes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
 
 
   return (
-    <div className="">
-     
+    <div className="content">
+
     </div>
   );
 }
---
+++ src/DomContent.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function DomContent(){
+
+
+    
+    
+    return(
+        <div>
+        <div id="cars"> Cars </div>
+
+        <div id="homes"> Homes</div>
+        </div>
+
+    )
+}
+
+export default DomContent;
